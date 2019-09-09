0e64cdbe257aa7b2a0accc0c662cb21926face3a
Tolik <tolik1794@gmail.com>
2019-09-09 17:35:38 +0300

    add resources and update userController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserCollection;
+// use App\Http\Resources\UserResource;
 use App\Models\User;
+use App\Http\Resources\User as UserResource;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -15,38 +18,42 @@ class UserController extends Controller
             ->orWhere('last_name', 'like', "%{$search}%")
             ->orWhere('country', 'like', "%{$search}%")
             ->orWhere('city', 'like', "%{$search}%")
-            ->get()
-            ->makeHidden(['api_token', 'email_verified_at', 'organization'])
-            ->load(['organization']);
-        return response()->json([
-            'success' => true,
-            'data' => $users
-        ], 200);
+            ->get();
+            // ->makeHidden(['api_token', 'email_verified_at', 'organization'])
+            // ->load(['organization']);
+        // return response()->json([
+        //     'success' => true,
+        //     'data' => $users
+        // ], 200);
+        return new UserCollection($users);
     }
 
     public function show(User $user)
     {
-        return response()->json([
-            'success' => true,
-            'data' => $user
-        ], 200);
+        // return response()->json([
+        //     'success' => true,
+        //     'data' => $user
+        // ], 200);
+        return new UserResource($user);
     }
 
     public function update(Request $request, User $user)
     {
         $user->update($request->all());
-        return response()->json([
-            'success' => true,
-            'data' => $user
-        ], 200);
+        // return response()->json([
+        //     'success' => true,
+        //     'data' => $user
+        // ], 200);
+        return new UserResource($user);
     }
 
     public function destroy(User $user)
     {
         $user->delete();
-        return response()->json([
-            'success' => true,
-            'data' => $user
-        ], 204);
+        // return response()->json([
+        //     'success' => true,
+        //     'data' => $user
+        // ], 204);
+        return new UserResource($user);
     }
 }
